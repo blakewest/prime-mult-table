373c508dbc3091d08752097ff4d0ee4010b1a2c3
Blake West <bwest87@gmail.com>
2014-02-08 17:09:21 -0800

    added initial make_table function and renamed functions to be more idiomatic

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-def isPrime?(num)
+def is_prime?(num)
   # Handle edge cases
   return true if num == 2
   return false if num % 2 == 0 or num <= 1
@@ -14,16 +14,33 @@ def isPrime?(num)
   return true
 end
 
-def findNPrimes(n)
+def find_n_primes(n)
   return if n <= 0
 
-  #Just start with 2 so we can start with 3 and count by 2's when trying new numbers to place in result array
+  #Just start with 2 so we can start trying numbers at 3 and count by 2's when trying new numbers to place in result array
   result = [2]
   curNum = 3
   while result.length < n
-    result.push(curNum) if isPrime?(curNum)
+    result.push(curNum) if is_prime?(curNum)
     curNum += 2
   end
 
   return result
+end
+
+def make_table(nums)
+    table = ''
+    nums.unshift(1)
+    nums.each_with_index do |num1, i1|
+        row = ''
+        nums.each_with_index do |num2, i2|
+            if i1 == 0 and i2 == 0
+                row += "|   "
+            else
+                row += "| #{num1*num2} "
+            end
+        end
+        table += row + "|\n"
+    end
+    puts table
 end
