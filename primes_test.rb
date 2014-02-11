5934d0415dc946812304ac0dc7ed577bee1782cb
Blake West <bwest87@gmail.com>
2014-02-11 09:29:52 -0800

    added setup functions for each test class, and added a test for negative numbers

--- primes_test.rb
+++ primes_test.rb
@@ -2,10 +2,11 @@ require 'test/unit'
 require_relative './primes'
 
 class TestPrimesIsPrimeMethod < Test::Unit::TestCase
+  def setup
+    @primes = Primes.new
+  end
 
   def test_prime_cases
-    @primes = Primes.new
-
     #Some small and large primes
     assert @primes.is_prime?(2)
     assert @primes.is_prime?(11)
@@ -13,8 +14,6 @@ class TestPrimesIsPrimeMethod < Test::Unit::TestCase
   end
 
   def test_composite_cases
-    @primes = Primes.new
-
     #Some small and large composites
     assert !@primes.is_prime?(4)
     assert !@primes.is_prime?(49)
@@ -23,16 +22,16 @@ class TestPrimesIsPrimeMethod < Test::Unit::TestCase
 end
 
 class TestPrimesFindNPrimesMethod < Test::Unit::TestCase
-  def test_values_above_zero
+  def setup
     @primes = Primes.new
+  end
 
+  def test_values_above_zero
     assert_equal(@primes.find_n_primes(3), [2,3,5])
     assert_equal(@primes.find_n_primes(5), [2,3,5,7,11])
   end
 
   def test_values_below_or_equal_0
-    @primes = Primes.new
-
     assert_equal(@primes.find_n_primes(0), nil)
     assert_equal(@primes.find_n_primes(-1), nil)
     assert_equal(@primes.find_n_primes(-3), nil)
@@ -40,17 +39,22 @@ class TestPrimesFindNPrimesMethod < Test::Unit::TestCase
 end
 
 class TestPrimesCreateNPrimesTableMethod < Test::Unit::TestCase
-  def test_default_ten_primes
+  def setup
     @primes = Primes.new
+  end
 
+  def test_default_ten_primes
     #See if resulting table string contains the square of 11 (121). It should if all cells are getting made
     assert @primes.create_n_primes_table(10).include? "121"
   end
 
   def test_can_take_arguments_to_make_larger_tables
-    @primes = Primes.new
-
     #See if the resulting string contains the 12th prime
     assert @primes.create_n_primes_table(12).include? "37"
+  end
+
+  def test_negative_numbers
+    #Should return nothing if negative number is given. (A message is printed to console if actually run)
+    assert_nil @primes.create_n_primes_table(-5)
   end
 end
